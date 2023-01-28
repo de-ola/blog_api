84358f1288d1446d064bb9df3bc50aaac6a0d4b3
de-ola <ademiluaadeola969@gmail.com>
2023-01-28 19:29:27 +0100

    added the blog, category and comments model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from user.models import User
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=10000)
+
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100000)
+    category = models.ForeignKey(Category, related_name="category", on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name="author", on_delete=models.CASCADE)
+    snippet = models.TextField()
+    body = RichTextField()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=1000)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.blog.title
