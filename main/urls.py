6628f3f8fee835bae8685def00f54f098cea85fa
de-ola <ademiluaadeola969@gmail.com>
2023-01-29 16:21:50 +0100

    updated the main views and urls

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('category/', CategoryView.as_view(), name="category_list"),
     path('category/<int:id>/', BlogCategoryView.as_view(), name="filtered_blog_list"),
     path('blog/', BlogListView.as_view(), name="blog_list"),
+    path('blog/<int:id>/', BlogDetailView.as_view(), name="blog_post"),
+    path('create_post/', BlogCreateView.as_view(), name="create_blog"),
 ]
--- main/views.py
+++ main/views.py
@@ -21,3 +21,23 @@ class BlogListView(generics.ListAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogListSerializer
     permission_classes = [permissions.AllowAny]
+
+class BlogCreateView(generics.CreateAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = AddBlogPostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class BlogDetailView(APIView):
+    def get(self, request, format=None, **kwargs):
+        blog = Blog.objects.filter(id = kwargs["id"]).first()
+        serializer = BlogPostSerializer(instance=blog)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = AddCommentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
